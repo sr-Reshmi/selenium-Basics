c19c17bc4baf8a16bac043e062f6613688d82724
reshmi <reshmisrajendra@gmail.com>
2024-08-21 20:24:36 +0530

    first

---
+++ Example.java
@@ -0,0 +1,19 @@
+package selenium1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Example {
+
+	public static void main(String[] args) {
+	//System.setProperty("webdriver.chrome.driver","C:\\Users\\reshm\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://www.google.com/");
+		
+		
+		
+		
+		
+
+	}
+
+}
